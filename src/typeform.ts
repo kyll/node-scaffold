d8f5f8f7a37139859d29d230ceccb9076233f49c
Maartje <maartje.snoeckhenkemans@gmail.com>
2016-10-13 15:38:40 +0200

    made sure responses come in in json format

--- src/typeform.ts
+++ src/typeform.ts
@@ -2,14 +2,15 @@
  * Created by Maartje on 13/10/16.
  */
 import request from 'request';
-import Promise from 'bluebird';
+// import Promise from 'bluebird';
+
 export class Typeform  {
     constructor(key) {
         this.key = key;
     }
     private key;
-    private baseFormUrl = 'https://api.typeform.com/v1/form/';
-    private baseFormsUrl = 'https://api.typeform.com/v1/forms';
+    private singleFormUrl = 'https://api.typeform.com/v1/form/';
+    private multiFormsUrl = 'https://api.typeform.com/v1/forms';
     private keyUrl = '?key=';
 
     private _request(url) {
@@ -26,24 +27,27 @@ export class Typeform  {
         })
     }
 
-    public getForms() {
-        const fullUrl = this.baseFormsUrl.concat(this.keyUrl, this.key);
-        return this._request(fullUrl);
+    public async getForms() {
+        const fullUrl = this.multiFormsUrl.concat(this.keyUrl, this.key);
+        const response = await this._request(fullUrl);
+        return JSON.parse(response);
     }
 
-    public getForm (formId) {
-        const fullUrl = this.baseFormUrl.concat(formId, this.keyUrl, this.key);
-        return this._request(fullUrl);
+     public async getForm (formId) {
+        const fullUrl = this.singleFormUrl.concat(formId, this.keyUrl, this.key);
+        const response = await this._request(fullUrl);
+        return JSON.parse(response);
+
     }
 
 }
 
-// const typeForm = new Typeform('451eb37af49fae6b347b3d143082fa8d9b5c272c');
-//
-// typeForm.getForm('yWoKCp').then(data => {
-//     console.log(data);
-// });
-//
-// typeForm.getForms().then(data => {
-//     console.log(data);
-//});
+const typeForm = new Typeform('451eb37af49fae6b347b3d143082fa8d9b5c272c');
+
+typeForm.getForm('yWoKCp').then(data => {
+    console.log(data);
+});
+
+typeForm.getForms().then(data => {
+    console.log(data);
+});
